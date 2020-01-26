b553a3c7246f16e7d4d823640c2264d505d7d505
don.jasto <jakov.stosic@gmail.com>
2020-01-26 23:52:14 +0100

    Add project files.

---
+++ MovieRental/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRental.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+
+        //Foreign key
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
