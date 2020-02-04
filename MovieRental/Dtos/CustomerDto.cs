fb7c8ecdbe583b63665d82321d7914187eac12bd
don.jasto <jakov.stosic@gmail.com>
2020-02-04 16:25:56 +0100

    Create Customer Dto and map domain object with Automapper

---
+++ MovieRental/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRental.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        //Foreign key
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
