01f7e552aad718aabeffe72b43f05ba85ed814d2
don.jasto <jakov.stosic@gmail.com>
2020-02-08 23:05:55 +0100

    Added client side code ajax and datatables to collect info from customer api , server side code removed

--- MovieRental/Controllers/CustomersController.cs
+++ MovieRental/Controllers/CustomersController.cs
@@ -74,9 +74,9 @@ namespace MovieRental.Controllers
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
