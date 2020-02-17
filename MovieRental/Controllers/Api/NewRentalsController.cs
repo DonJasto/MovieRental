6d96050f2ea2767f8d29954ec25a3f5ca1ddd3b9
don.jasto <jakov.stosic@gmail.com>
2020-02-17 22:40:06 +0100

    Updated CreateNewRentals controller

--- MovieRental/Controllers/Api/NewRentalsController.cs
+++ MovieRental/Controllers/Api/NewRentalsController.cs
@@ -22,10 +22,13 @@ namespace MovieRental.Controllers.Api
         {
             var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
 
-            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental 
